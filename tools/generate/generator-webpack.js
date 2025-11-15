077c465a200b85ac5a82b1955dee364f7f86cc8a
frank <frank@server-wizard.com>
2025-11-14 17:02:05 -0700

    Update deps. Add webpacking of assembler and generator.

---
+++ tools/generate/generator-webpack.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const {merge} = require('webpack-merge');
+
+const baseConfigFn = require(path.resolve('scripts', 'wp-common'));
+const modeConfigFn = require(path.resolve('scripts', 'wp-mode'));
+
+const config = {
+	entry: path.join(__dirname, 'src/main.ts'),
+	target: 'node',
+	output: {
+		path: path.resolve('dist'),
+		libraryTarget: 'commonjs2',
+	},
+};
+
+const tsconfigFilePath = process.env.TS_NODE_PROJECT || path.resolve(__dirname, 'tsconfig.json');
+module.exports = merge(
+	baseConfigFn(tsconfigFilePath),
+	modeConfigFn('development', 'generator.js'),
+	config
+);
